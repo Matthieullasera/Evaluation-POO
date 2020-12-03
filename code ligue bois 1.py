f6c2cf9a8bfc1792494bc0c2d59551233a3ab572
mattllas <59658376+mattllas@users.noreply.github.com>
2020-12-03 15:43:07 +0100

    Update code ligue bois 1.py

--- code ligue bois 1.py
+++ code ligue bois 1.py
@@ -9,7 +9,7 @@ import numpy as np
 # game loop
 while True:
     action_count = int(input())  # the number of spells and recipes in play
-    tab_command= []  
+    tab_command = []
     tab_sort = []
     tab_inv = []
     for i in range(action_count):
@@ -24,7 +24,19 @@ while True:
         # tax_count: in the first two leagues: always 0; later: the amount of taxed tier-0 ingredients you gain from learning this spell
         # castable: in the first league: always 0; later: 1 if this is a castable player spell
         # repeatable: for the first two leagues: always 0; later: 1 if this is a repeatable player spell
-        action_id, action_type, delta_0, delta_1, delta_2, delta_3, price, tome_index, tax_count, castable, repeatable = input().split()
+        (
+            action_id,
+            action_type,
+            delta_0,
+            delta_1,
+            delta_2,
+            delta_3,
+            price,
+            tome_index,
+            tax_count,
+            castable,
+            repeatable,
+        ) = input().split()
         action_id = int(action_id)
         delta_0 = int(delta_0)
         delta_1 = int(delta_1)
@@ -36,19 +48,14 @@ while True:
         castable = castable != "0"
         repeatable = repeatable != "0"
         etat = 0
-        
-        
-        if action_type == "BREW" :  #tableau pour stocker toutes les commandes
 
-            tab_command.append((price,action_id,[delta_0,delta_1,delta_2,delta_3]))
+        if action_type == "BREW":  # tableau pour stocker toutes les commandes
+
+            tab_command.append((price, action_id, [delta_0, delta_1, delta_2, delta_3]))
             tab_command.sort()
+        if action_type == "CAST":  # tableau pour stocker les sorts
 
-        
-        if action_type == "CAST" : #tableau pour stocker les sorts
-            
-            tab_sort.append((action_id,castable,(delta_0,delta_1,delta_2,delta_3)))
-    
-   
+            tab_sort.append((action_id, castable, (delta_0, delta_1, delta_2, delta_3)))
     for i in range(2):
         # inv_0: tier-0 ingredients in inventory
         # score: amount of rupees
@@ -56,75 +63,82 @@ while True:
         tab_inv.append([inv_0, inv_1, inv_2, inv_3])
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
-
     best_id = tab_command[2]
     el_b = best_id[2][0] + tab_inv[0][0]
     el_v = best_id[2][1] + tab_inv[0][1]
     el_o = best_id[2][2] + tab_inv[0][2]
     el_j = best_id[2][3] + tab_inv[0][3]
 
-    
-    if el_b <= 0 :
+    if el_b < 0 and tab_inv[0][0] == 0:
 
-
-        if tab_sort[0][1] == 1 :  
+        if tab_sort[0][1] == 1:
             a = "CAST"
-            b = tab_sort[0][0]
-
-        else : 
+            b = 78
+        else:
             a = "REST"
             b = ""
+    if el_v < 0 and tab_inv[0][0] > 0:
 
-    
-    if el_v <= 0 and tab_inv[0][0] > 0:
-
-
-        if tab_sort[1][1] == 1 : 
+        if tab_sort[1][1] == 1:
             a = "CAST"
-            b = tab_sort[1][0]
-
-        else : 
+            b = 79
+        else:
             a = "REST"
             b = ""
-    
-        
-    
-    if el_o <= 0 and tab_inv[0][1] > 0:
-       
-        
-        if tab_sort[2][1] ==1:
+    elif tab_inv[0][0] == 0:
+        if tab_sort[0][1] == 1:
             a = "CAST"
-            b = tab_sort[2][0]
-        else : 
+            b = 78
+        else:
             a = "REST"
             b = ""
+    if el_o < 0 and tab_inv[0][1] > 0:
 
-
-
-    if el_j <= 0 and tab_inv[0][2] > 0:
-
-        
-        if tab_sort[3][1]==1:
+        if tab_sort[2][1] == 1:
             a = "CAST"
-            b = tab_sort[3][0]
-
-        else :
+            b = 80
+        else:
             a = "REST"
             b = ""
+    elif tab_inv[0][1] == 0:
 
-  
-    tab_res = [el_b,el_v,el_o,el_j]
-    if (el_b) >=0 and (el_v) >=0 and (el_o) >=0 and (el_j) >=0 :
+        if tab_sort[1][1] == 1:
+            a = "CAST"
+            b = 79
+        else:
+            a = "REST"
+            b = ""
+    elif tab_inv[0][0] == 0:
+
+        if tab_sort[0][1] == 1:
+            a = "CAST"
+            b = 78
+        else:
+            a = "REST"
+            b = ""
+    if el_j < 0 and tab_inv[0][2] > 0:
+
+        if tab_sort[3][1]:
+            a = "CAST"
+            b = 81
+        else:
+            a = "REST"
+            b = ""
+    elif tab_inv[0][2] < 0:
+
+        if tab_sort[2][1] == 1:
+            a = "CAST"
+            b = 80
+        else:
+            a = "REST"
+            b = ""
+    tab_res = [el_b, el_v, el_o, el_j]
+    if (el_b) >= 0 and (el_v) >= 0 and (el_o) >= 0 and (el_j) >= 0:
         a = "BREW"
         b = best_id[1]
-    
-
-    
     # in the first league: BREW <id> | WAIT; later: BREW <id> | CAST <id> [<times>] | LEARN <id> | REST | WAIT
-    #print("WAIT")
-    
-    print(a,b)
-    print(tab_sort[0][0], file=sys.stderr, flush=True)
-    print(tab_res, file=sys.stderr, flush=True)
-    
+    # print("WAIT")
 
+    print(a, b)
+    print(tab_command, file=sys.stderr, flush=True)
+    print(tab_res, file=sys.stderr, flush=True)
